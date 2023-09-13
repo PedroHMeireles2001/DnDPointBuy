575525e865da690ed81a6359735f846a9df3150c
pedro <pedro-mmh@hotmail.com>
2023-09-13 16:08:52 -0300

    Align itens

--- src/components/AttributeInput/index.js
+++ src/components/AttributeInput/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './AttributeInput.module.css'
 
 export default function AttributeInput({ attribute, value, onIncrease, onDecrease }) {
   return (
-    <div className="attribute-input">
-      <h3>{attribute}</h3>
-      <button onClick={onDecrease}>-</button>
-      <span>{value}</span>
-      <button onClick={onIncrease}>+</button>
+    <div className={styles.attributeinput}>
+      <h3 className={styles.title}>{attribute}</h3>
+      <button onClick={onDecrease} className={styles.attbutton}>-</button>
+      <span className={styles.value}>{value}</span>
+      <button onClick={onIncrease} className={styles.attbutton}>+</button>
     </div>
   );
 }
